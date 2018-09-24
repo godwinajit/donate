020b377e266e7d0d2184e0c35d93a6be4c3b840b
Gabriel Oliver <goliver@mindtrustlabs.com>
2018-09-24 00:08:56 -0400

    Removed the airpot project files.

--- airpot/sidebar-header.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * The Header Sidebar
- */
- if (! is_active_sidebar ( 'sidebar-header' )) { return; }
-?>
-<?php dynamic_sidebar( 'sidebar-header' ); ?>
--- airpot/wp-test.php
+++
@@ -1,5 +0,0 @@
-<?php include('header.php'); ?>
-	<div class="container">		<main id="main">
-			
-		</main>	</div>
-<?php include('footer.php'); ?>
